d820542f9cc590f464c4c156f37cbdbf6251e855
Assaf Shomer <assafshomer@gmail.com>
2015-03-15 22:56:49 +0200

    Added nicer tooltips, but they mess with the tds, so removed them from pixesl for now

---
+++ app/helpers/views_helper.rb
@@ -0,0 +1,10 @@
+module ViewsHelper
+	
+	def billboard_header
+		'The Blockchain BillBoard'
+	end
+
+	def billboard_title
+		billboard_header
+	end
+end
--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -1,1 +1,5 @@
-PIXELS = 100
+DEV = (Rails.env=='development')
+PROD = (Rails.env=='production')
+TEST = (Rails.env=='test')
+HEROKU = (Rails.root.to_path=="/app")
+PIXELS = if PROD then 1000 else 10 end
