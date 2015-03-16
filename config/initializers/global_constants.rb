8c3506ba6ac2fd20ed93f527bff343d9821adbe6
Assaf Shomer <assafshomer@gmail.com>
2015-03-16 08:42:42 +0200

    in prod 200x5=1000

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -2,4 +2,4 @@ DEV = (Rails.env=='development')
 PROD = (Rails.env=='production')
 TEST = (Rails.env=='test')
 HEROKU = (Rails.root.to_path=="/app")
-PIXELS = if PROD then 1000 elsif DEV then 50 elsif TEST then 5 end
+PIXELS = if PROD then 200 elsif DEV then 50 elsif TEST then 5 end
