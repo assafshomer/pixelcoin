752f15a082d376be0b232ca8577aab24e22149bf
Assaf Shomer <assafshomer@gmail.com>
2015-03-18 15:58:28 +0200

    Global constats for prod

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -3,4 +3,4 @@ PROD = (Rails.env=='production')
 TEST = (Rails.env=='test')
 HEROKU = (Rails.root.to_path=="/app")
 # PIXELS = if PROD then {x:250, y:160} elsif DEV then {x:25, y:16} else {x:3, y:3} end
-PIXELS = if TEST then {x:3, y:3} else {x:APP_CONFIG['columns'],y:APP_CONFIG['rows']} end
+PIXELS = if PROD then {x:1250, y:800} else {x:APP_CONFIG['columns'],y:APP_CONFIG['rows']} end
