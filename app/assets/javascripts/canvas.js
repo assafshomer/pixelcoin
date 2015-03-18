04ca64859d980939fd281951440639f33b22dc46
Assaf Shomer <assafshomer@gmail.com>
2015-03-18 14:34:56 +0200

    canvas selection working

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -5,8 +5,8 @@ var fin_y = 0;
 var track = 0;
 function getPos(e){
 		if (track > 0) {
-	    x=e.clientX;
-	    y=e.clientY;
+	    x=e.clientX- rect.left;
+	    y=e.clientY- rect.top;
 	    x_min = Math.min(init_x, x);
 	    x_max = Math.max(init_x, x);
 	    y_min = Math.min(init_y, y);
@@ -28,15 +28,15 @@ function stopTracking(){
 $( document ).ready(function() {  
 	$("#billboard").mousedown(function(e) {
 		track = 1;
-    init_x=e.clientX;
-    init_y=e.clientY;
+    init_x=e.clientX- rect.left;
+    init_y=e.clientY- rect.top;
     cursor="Your Mouse Position Is : " + init_x + " and " + init_y ;
     document.getElementById("initpos").innerHTML=cursor;    
 	});
 	$("#billboard").mouseup(function(e) {
 		track = 0;
-    fin_x=e.clientX;
-    fin_y=e.clientY;
+    fin_x=e.clientX- rect.left;
+    fin_y=e.clientY- rect.top;
     cursor="Your Mouse Position Is : " + fin_x + " and " + fin_y ;
     document.getElementById("finpos").innerHTML=cursor;
 	});
@@ -45,4 +45,4 @@ $( document ).ready(function() {
 function clearCanvas(){
 	ctx.fillStyle = '#bbb';
 	ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
-}
+};
